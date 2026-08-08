16df2170c52bcace5f424e9a4eb7dbadf1f327d9
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-08-09 00:16:43 +0530

    Question 1). Create a Guessing Number game.

--- Java Script Apna College/Javascript_Practice-Series/practice.js
+++ Java Script Apna College/Javascript_Practice-Series/practice.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+
+// Question 1). Create a Guessing Number game. 
+
+let gameNum = 25;
+let userNum = prompt("Guess the game number");
+
+while(userNum != gameNum){   //game 
+    userNum = prompt("Try it Second Number, Guess Again : ");
+    // if(userNum == gameNum){
+    //     break;
+    // }
+
+}
+
+console.log("Congratulation you guessed the Number : ",userNum);
+
+
+
+
