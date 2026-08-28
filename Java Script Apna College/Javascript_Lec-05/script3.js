a69a036e2ac15f716fc33e6f12988eb0b9d8063c
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-08-28 20:17:21 +0530

    Understood Arrow function creation and passed arguments in it same as parametize function with practice problem

--- Java Script Apna College/Javascript_Lec-05/script3.js
+++ Java Script Apna College/Javascript_Lec-05/script3.js
@@ -99,6 +99,8 @@ let val = mySum(20 , 40); //c returns its value in val variable.
 console.log("sum from return keyword with c: " +(val));//60
 
 
+// Starting Arrow function using both let and const variable.
+
 //Arrow Functions; it is the part of Modern javascript.
 //Compact way of writing function in javascript;
 //syntax : let functionName = (parameters) => {
