1ea9f3d03139f5081ab38ee2280531a8a5d8f3a7
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-09-07 21:22:51 +0530

    manipulating the DOM elements using javascript code

--- Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
+++ Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
@@ -2,4 +2,4 @@ console.log("DOM --> Document Object Model");
 // start DOM.
 // adding three files in a folder html,css,js and connects with html boiler plate code.
 // to study DOM we need to create a folder and add three files in it.
-// Learned new topics in DOM of adding functioning to our website 
+// Learned new topics in DOM of adding functioning to our website --> through server side running javascript code in browser console & manipulating the DOM elements using javascript code.
