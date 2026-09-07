05053cbe81821ac1dbeb95cbfa333614a7438706
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-09-07 21:21:03 +0530

    DOM of adding functioning to our website

--- Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
+++ Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
@@ -2,3 +2,4 @@ console.log("DOM --> Document Object Model");
 // start DOM.
 // adding three files in a folder html,css,js and connects with html boiler plate code.
 // to study DOM we need to create a folder and add three files in it.
+// Learned new topics in DOM of adding functioning to our website 
