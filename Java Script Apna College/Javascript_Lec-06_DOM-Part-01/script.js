da2b739ebbc36ddb6378ab627d11e6e603d66998
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-09-09 23:39:25 +0530

    adding alerts to website using DOM and Concept of Window object

--- Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
+++ Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
@@ -3,3 +3,21 @@ console.log("DOM --> Document Object Model");
 // adding three files in a folder html,css,js and connects with html boiler plate code.
 // to study DOM we need to create a folder and add three files in it.
 // Learned new topics in DOM of adding functioning to our website --> through server side running javascript code in browser console & manipulating the DOM elements using javascript code.
+
+// 
+// The 3 Musketeers of Web Dev --> HTML(Structure)  CSS(Style)  JavaScript(Logic).
+// Stater Code : -
+// <style> tag connects HTMl with CSS
+// <script> tag connects HTML with JavaScript.
+// alag se js ki  file banane se readability badti hai and modularity -> | html |--| css |--| js | .
+// browser caching.
+
+alert("Apna College");
+
+// #Window Object:-
+//--> The window object represents an open window in a browser. it is browser's 
+//     object(not JavaScript's) & is automatically created by browser.
+//--> It is a global object with lots of properties & methods.
+// 
+
+
