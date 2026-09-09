c2f3e612fd284938fbc8abc564432bbf5d0f3bbb
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-09-10 00:01:46 +0530

    DOM- Diagram as Introduction and Overview of DOM

--- Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
+++ Java Script Apna College/Javascript_Lec-06_DOM-Part-01/script.js
@@ -12,12 +12,54 @@ console.log("DOM --> Document Object Model");
 // alag se js ki  file banane se readability badti hai and modularity -> | html |--| css |--| js | .
 // browser caching.
 
-alert("Apna College");
+// alert("Apna College");
 
 // #Window Object:-
 //--> The window object represents an open window in a browser. it is browser's 
 //     object(not JavaScript's) & is automatically created by browser.
 //--> It is a global object with lots of properties & methods.
+// console.log is a method of window object. so we can write console.log() or window.console.log() both are same.
+// ex:-
+console.log("Hello World"); //same work
+window.console.log("Hello World2"); // same work
+window.alert("Hello World3");
+
+
+// ## What is DOM ?
+// --> When a web page is loaded, the browser creates a Document Object Model (DOM) of the page.
+// # Diagram : - 
+//                                        ----------
+//                                        | window |
+//                                        ----------
+//                                             |
+//                                         ----------
+//                                         |document|
+//                                         ----------
+//                                              |
+//                                          --------
+//                                          | html |
+//                                          --------
+//                                              |
+//                      ----------------------------------------------
+//                      |                                            |
+//                    --------                                    -------
+//                    | head |                                   | body |
+//                    --------                                   --------
+//                       |                                           |
+//      ----------------------------------               ------------------------ 
+//      |          |          |          |               |                      |
+//  --------    --------   --------   --------         -------               ----------
+//  | meta |    | meta |  | title |   | link |         | div |               | script |
+//  --------    --------  ---------   --------         -------               ----------
+//                                                       |
+//                                           ----------------------------
+//                                           |        |         |        |
+//                                        -------   ------    -----   -------
+//                                        | img |   | h2 |    | p |   | div |
+//                                        -------   ------    -----   -------
 // 
+// 
+// 
+
 
 
