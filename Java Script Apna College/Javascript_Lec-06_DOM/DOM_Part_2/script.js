a095c8c9a3cf0b7dd6adcbe3f262462ed967a3b9
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-09-25 23:36:07 +0530

    Starting_DOM_part_02_understand_new_concepts

---
+++ Java Script Apna College/Javascript_Lec-06_DOM/DOM_Part_2/script.js
@@ -0,0 +1,26 @@
+// DOM --> Document Object Model --> Part_2-(two)
+console.log("DOCUMENT OBJECT MODEL PART-02");
+// DOM - Manupulation : - 
+
+// # Attributes : -
+// 1). getAttribute(attr) // to get the attributive value.
+
+// 2). setAttribute(attr, value) // to set the attributive value.
+
+// # Style : -
+// --> node.style
+
+// div --> node.
+//  eg :-
+
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+let para = document.querySelector("p");
+console.log();
